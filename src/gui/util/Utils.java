f3879dc9ccc06364bf9b0f9136b56c1b8e7156e7
GustavoRPaes <gustavo.rp@aluno.ifsc.edu.br>
2022-01-25 12:15:50 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -7,6 +7,14 @@ import javafx.stage.Stage;
 public class Utils {
 
 	public static Stage currentStage(ActionEvent event) {
-		return (Stage) ((Node)event.getSource()).getScene().getWindow();
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+	}
+
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
 	}
 }
